a98acfb73ae157861cc238af52a86259a8726856
saboalfazl6162 <saboalfazl6162@gmail.com>
2025-11-10 15:51:45 +0330

    front 1.2

--- apps/upload_admin/views.py
+++ apps/upload_admin/views.py
@@ -2,5 +2,4 @@ from django.views.generic import TemplateView
 from django.http import JsonResponse
     
 class AdminPageView(TemplateView):
-    template_name = "admin_page.html"
-    
+    template_name = "base.html"
--- markaz_bargozari/settings.py
+++ markaz_bargozari/settings.py
@@ -108,6 +108,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIR = (
     os.path.join(BASE_DIR,'static')
 )
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'uploads')
 MEDIA_URL = '/uploads/'
